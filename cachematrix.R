586fe8ad132fbbba91e75bf171202c74b3a96cef
JTScoggins <JTS@Scoggins.Net>
2014-04-27 22:40:45 +0700

    It seems to be working now

--- cachematrix.R
+++ cachematrix.R
@@ -2,24 +2,33 @@
 ##
 ##  This code is a derivative of the makeVector and cachemean functions in the course
 ##  Programming Exercise 2 example.  Those examples have been modified to handle matricies
-##  instead of numeric vectors.
-##
-##  makeCacheMatrix() function allows for creation retrieval of original and inverted matrix
-##  cacheSolve() pulls inverted matrix from cache, otherwise solves inverted matrix and caches
+##  instead of numeric vectors. Replace mean with the solve() function to invert the matrix.
+##  Became real clear after reading Fu Sheng Wang's explaination and test case in the 
+##  discussion forum and googling to find the solve() function to invert the matrix.
 
-##  makeCacheMatrix() functions:
-##  set: usage example: cx$setMatrix(matrix(5:8,2))
+## Test Case to see if it works:
+## > a <- makeCacheMatrix()
+## > a$set(matrix(rnorm(16), 4) )
+## > a$get()          ## should see matrix with random numbers
+## > a$getInv()       ## should see NULL, not in cache yet
+## > cacheSolve(a)    ## should see inverted Matrix
+## > cacheSolve(a)    ## should see cache message and inverted matrix
+## > a$getInv()       ## now inverted matrix is in cache can get from here too
+
+##  makeCacheMatrix() functionality:
+##  set: sets the original matrix to be inverted in cacheSolve()
 ##  get: shows the matrix object created with the set
-##  setInv: stores the inverted matrix solved in  
+##  setInv: stores the inverted matrix solved in cacheSolve()
 ##  getInv: shows the inverted matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
      
-    ## invMatrix <- NULL     ## init Inverse matrix object
+     invMatrix <- NULL ## init Inverse matrix object, maybe redundant b/c cache is init'd in set
+                         ## works if you take it out but makeVector example does this too ...
      
      set <- function(y){  ## used create cache matrix object, init Inverse Matrix cache
           x <<- y
-          invMatrix <<- NULL
+          invMatrix <<- NULL  ## init Inverse matrix cache
      }
      
      get <- function() {   ## Return the matrix created
@@ -45,11 +54,19 @@ makeCacheMatrix <- function(x = matrix()) {
 } ## end makeCacheMatrix()
 
 
-##  cacheSolve functionality
+##  cacheSolve() functionality
 ##  checks to see if there is a matrix in cache already
 ##  If not, then invert the matrix and place into cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     inv <- x$getInv()
+     if (!is.null(inv)) {
+          message("Returning Cached Matrix, saving CPU cycles one step at a time :-)")
+          return(inv)
+     }
+     data <-x$get()  ## put the original matrix into data and pass to solve
+     inv <- solve(data, ...)
+     x$setInv(inv)
+     return(inv)
      
-}
+} ##  end cacheSolve()
